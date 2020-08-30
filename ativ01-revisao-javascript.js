58b2c8e8756ba3a5ba6b30c826eb4da63b1c2dd5
vimarteleto <vini_marteleto@live.com>
2020-08-30 23:03:14 +0000

    Atividade 30/08

--- ativ01-revisao-javascript.js
+++ ativ01-revisao-javascript.js
@@ -31,15 +31,89 @@ estadosNe.push(estado1)
       ao vetor estadosNe usando push().
 */
 
+let estado2 = {
+   nome: 'Alagoas',
+   sigla: 'AL',
+   area: 27848.14,
+   populacao: 3322820
+}
+
+estadosNe.push(estado2)
+
+let estado3 = {
+   nome: 'Rio Grande do Norte',
+   sigla: 'RN',
+   area: 52811.05,
+   populacao: 3479010
+}
+
+estadosNe.push(estado3)
+
+let estado4 = {
+   nome: 'Paraíba',
+   sigla: 'PB',
+   area: 56469.78,
+   populacao: 3996496
+}
+
+estadosNe.push(estado4)
+
+let estado5 = {
+   nome: 'Pernambuco',
+   sigla: 'PE',
+   area: 98148.32,
+   populacao: 9496294
+}
+
+estadosNe.push(estado5)
+
+let estado6 = {
+   nome: 'Ceará',
+   sigla: 'CE',
+   area: 148920.47,
+   populacao: 9075649
+}
+
+estadosNe.push(estado6)
+
+let estado7 = {
+   nome: 'Piauí',
+   sigla: 'PI',
+   area: 251577.74,
+   populacao: 3264531
+}
+
+estadosNe.push(estado7)
+
+let estado8 = {
+   nome: 'Maranhão',
+   sigla: 'MA',
+   area: 331937.45,
+   populacao: 7035055
+}
+
+estadosNe.push(estado8)
+
+let estado9 = {
+   nome: 'Bahia',
+   sigla: 'BA',
+   area: 564733.18,
+   populacao: 14812617
+}
+
+estadosNe.push(estado9)
+
 /*
    2) Escreva uma arrow function que receba dois parâmetros, área e população, e
       calcule a densidade demográfica retornando o resultado da divisão da
       segunda pela primeira. 
 */
 
+let densDemo = (area, populacao) => populacao / area
+
 /*
    3) Percorra o vetor estadosNe usando um for tradicional. Para cada estado,
-      chame a arrow function criada em 2) e efetue o cálculo correspondente.
+      chame a arrow function criada em 2)  efetue o cálculo correspondente.
       Com o resultado do cálculo, crie uma propriedade chamada 'densidade
       demográfica' para cada objeto representando um Estado.
 
@@ -47,16 +121,32 @@ estadosNe.push(estado1)
 
 */
 
+for(let i = 0; i < estadosNe.length; i++) {
+    let densidadeDemografica = densDemo(estadosNe[i].area, estadosNe[i].populacao)
+    estadosNe[i]['densidade demográfica'] = densidadeDemografica
+    delete estadosNe[i].sigla
+}
+
 /* 4) Escreva uma arrow function que receba um objeto. Na função, use for..in
       para extrair as propriedades e seus valores e exibi-los com console.log().
 
 */
 
+let exibir = estado => {
+    for(let info in estado) {
+        console.log('Propiedade: ${info}; Valor: ${estado[info]}')
+    }
+}
+
 /* 5) Percorra o vetor estadosNe usando for..of. Para cada objeto no vetor,
       invoque a função escrita em 4) para exibi-lo.
+*/
 
-/*
-   6)
+for(let estado of estadosNe) {
+    exibir(estado)
+}
+
+/*   6)
       a) Declare um vetor vazio.
       b) Insira no vetor criado no item a) apenas o nome de cada Estado, conforme
          os dados no topo deste arquivo, um de cada vez. Faça as inserções de 
@@ -65,5 +155,15 @@ estadosNe.push(estado1)
          deve ser inserido com unshift(), para manter a ordem alfabética. 
          O terceiro Estado (Rio Grande do Norte) deve ser inserido entre os dois 
          já existentes, e assim por diante.
-
 */
+
+let vetorEstados = []
+vetorEstados.push('Sergipe')
+vetorEstados.unshift('Alagoas')
+vetorEstados.splice(1, 0, 'Rio Grande do Norte')
+vetorEstados.splice(1, 0, 'Paraíba')
+vetorEstados.splice(2, 0, 'Pernambuco')
+vetorEstados.splice(1, 0, 'Ceará')
+vetorEstados.splice(4, 0, 'Piauí')
+vetorEstados.splice(2, 0, 'Maranhão')
+vetorEstados.splice(1, 0, 'Bahia')
