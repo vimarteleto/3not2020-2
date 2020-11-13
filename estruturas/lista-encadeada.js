df9d60db62d326c8a450e1cd29317e0d0c0d1824
vimarteleto <viniciusmarteleto@gmail.com>
2020-11-13 20:21:49 -0300

    Lista encadeada

--- estruturas/lista-encadeada.js
+++ estruturas/lista-encadeada.js
@@ -30,3 +30,24 @@ console.log(lista.toString(), lista.size())
 // Inserção na posição 1
 lista.insertAt(1, 'preto')
 console.log(lista.toString(), lista.size())
+
+// Remoção do ultimo elemento
+let removido = lista.pop()
+console.log(lista.toString(), lista.size(), {removido})
+
+// Remoção do primeiro elemento com removeAt()
+removido = lista.removeAt(0)
+console.log(lista.toString(), lista.size(), {removido})
+
+// Remoção na posição 3:
+removido = lista.removeAt(3)
+console.log(lista.toString(), lista.size(), {removido})
+
+let p0 = lista.getAt(0)
+let p4 = lista.getAt(4)
+console.log(lista.toString(), lista.size(), {p0, p4})
+
+let posCinza = lista.indeOf('cinza') // Cinza nao existe, deve retornar -1
+let posAmarelo = lista.indeOf('amarelo')
+let posRosa = lista.indeOf('rosa')
+console.log(lista.toString(), lista.size(), {posCinza, posAmarelo, posRosa})
